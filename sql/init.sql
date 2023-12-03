33df65e09f0824b9dfa1b7b2a14fef0a0f766ed8
Jhonathan Mike <jhonathan.mke@gmail.com>
2023-12-03 10:57:47 -0300

    fix: init.sql procedure

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,12 @@
 -- ./sql/init.sql
 -- psql -h localhost -U admin -d clock_time_api -f ./sql/init.sql
 
+CREATE TABLE IF NOT EXISTS job (
+    id SERIAL PRIMARY KEY,
+    description_job VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 INSERT INTO job (description_job, created_at) VALUES ('ADMIN_1', NOW());
 INSERT INTO job (description_job, created_at) VALUES ('ADMIN_2', NOW());
 INSERT INTO job (description_job, created_at) VALUES ('WORKER_1', NOW());
